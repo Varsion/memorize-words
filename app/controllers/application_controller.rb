f1af82d784ba08ab67cf0cf208a56b7b99848477
Varsion <jianhual12@outlook.com>
2023-07-22 17:24:53 +0800

    core: set default response format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ErrorHandler
 
+  before_action :set_default_response_format
+
+  private
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
