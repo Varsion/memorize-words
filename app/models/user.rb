f779a9668cd4adf93fe9fc0f0dd79d86461f1677
Varsion <jianhual12@outlook.com>
2023-07-22 17:26:48 +0800

    feat: user related routes and model(missed)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
   has_secure_password
+
+  def login
+    JWT.encode({
+      user_id: id,
+      created_at: DateTime.now.strftime("%Q"),
+    }, Rails.application.credentials.secret_key_base)
+  end
 end
