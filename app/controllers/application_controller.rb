65632c46de94fb31f3c69b9d0227454f032472fc
Varsion <jianhual12@outlook.com>
2023-07-23 15:33:32 +0800

    feat: error_handler refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::API
 
   before_action :set_default_response_format
 
+  def authenticate_user!
+    raise UnauthorizedException.new unless set_current_user && @current_user
+  end
+
   private
+
+  def set_current_user
+    token = request.headers["Authorization"]
+    data = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+    @current_user = User.find_by(id: data["user_id"])
+  end
 
   def set_default_response_format
     request.format = :json
--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -1,13 +1,28 @@
 module ErrorHandler
-  extended ActiveSupport::Concern
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from(JWT::DecodeError, UnauthorizedException) { |err| handler_401(err: err) }
+  end
+
+  def handle_error(status:, err: nil, message: nil, fields: nil)
+    if err.is_a?(Exception)
+      message ||= err.message
+      fields ||= err.class
+    end
+    fields = Array.wrap(fields)
+    render json: {
+      message: message || "系统错误",
+      fields: fields,
+    }, status: status
+  end
 
   # params error
   def handle_422(message: nil, fields:)
-    fields = Array.wrap(fields)
-    render json: {
-             message: message || "参数不合法",
-             fields: fields,
-           },
-           status: 422
+    handler_error(status: 422, fields: fields, message: message || "参数不合法")
+  end
+
+  def handler_401(err:)
+    handle_error(status: 401, err: err, message: "用户权限校验失败")
   end
 end
