46362127b25a56df444635e182efff91bb83fc0d
Varsion <jianhual12@outlook.com>
2023-07-25 21:06:13 +0800

    spec: glossary add vocabulary

--- spec/models/glossary_spec.rb
+++ spec/models/glossary_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Glossary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "add vocabularies" do
+    glossary = create(:glossary)
+    vocabulary_1 = create(:vocabulary)
+    vocabulary_2 = create(:vocabulary)
+    glossary.add_vocabularies([vocabulary_1.id, vocabulary_2.id])
+    expect(glossary.vocabularies.count).to eq(2)
+  end
 end
