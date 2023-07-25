326204a481c1ed875059b9404d44d6651f2adc09
Varsion <jianhual12@outlook.com>
2023-07-25 21:48:14 +0800

    comment: user's default glossary error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe User, type: :model do
       email: "jianhua@test.com",
       password: "123456",
     )
+    # why i got two collect record
+    # it case to user have two same glossaries
+    expect(user.glossaries.uniq.count).to eq(1)
     expect(Glossary.count).to eq(1)
     expect(Glossary.first.title).to eq("jianhua 的 生词本")
     expect(Glossary.first.owner).to eq(user)
