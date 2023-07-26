5ca4f947a67cbc57166230211c1f17684d159ea7
Varsion <jianhual12@outlook.com>
2023-07-26 20:56:16 +0800

    feat: factory improve

--- spec/factories/vocabulary.rb
+++ spec/factories/vocabulary.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :vocabulary do
-    display { Faker::Games::DnD.monster }
+    display { Faker::Number.hexadecimal(digits: 3)  }
     description { Faker::Lorem.paragraph }
     pronunciation { Faker::Games::DnD.monster }
     translation { Faker::Games::DnD.alignment }
