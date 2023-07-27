a2ee13735e15d498ee393d90cd1d181244b4acbb
Varsion <jianhual12@outlook.com>
2023-07-27 20:23:07 +0800

    feat: return learning_state with vocabulary

--- app/views/shared/_vocabulary.json.jbuilder
+++ app/views/shared/_vocabulary.json.jbuilder
@@ -1,5 +1,7 @@
 json.extract! vocabulary, :id, :display, :secondly_display, :translation, :description, :pronunciation, :language, :updated_at
 
+json.learning_state vocabulary.user_state(@current_user.id)
+
 json.sentences do
   json.array! vocabulary.sentences, partial: "shared/sentence", as: :sentence
 end
--- spec/factories/vocabulary.rb
+++ spec/factories/vocabulary.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :vocabulary do
-    display { Faker::Number.hexadecimal(digits: 3)  }
+    display { Faker::Number.hexadecimal(digits: 5) }
     description { Faker::Lorem.paragraph }
     pronunciation { Faker::Games::DnD.monster }
     translation { Faker::Games::DnD.alignment }
